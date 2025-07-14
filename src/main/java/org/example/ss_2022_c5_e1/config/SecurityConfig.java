3fe9c98d21004d13451955859679e270fd21d23d
@oana.novac <novac.oana@yahoo.com>
2025-07-14 16:01:57 +0300

    Adding authorities and roles

--- src/main/java/org/example/ss_2022_c5_e1/config/SecurityConfig.java
+++ src/main/java/org/example/ss_2022_c5_e1/config/SecurityConfig.java
@@ -20,7 +20,13 @@ public class SecurityConfig {
                 .authorizeRequests()
 //                .anyRequest().authenticated() // endpoint level authorization
                 // the request can be denied completely even before it reaches the controller
-                .anyRequest().permitAll()
+//                .anyRequest().permitAll()
+//                .anyRequest().denyAll()
+//                .anyRequest().hasAuthority("read")
+//                .anyRequest().hasAnyAuthority("read", "write")
+//                .anyRequest().hasRole("ADMIN")
+//                .anyRequest().hasAnyRole("ADMIN", "MANAGER")
+                .anyRequest().access("")
                 .and().build();
 
         // .anyRequest().authenticated() === matcher method + authorization rule
@@ -34,9 +40,24 @@ public class SecurityConfig {
 
         var u1 = User.withUsername("bill")
                 .password(passwordEncoder().encode("12345"))
-                .authorities("read").build();
+//                .authorities("ROLE_ADMIN")
+                .authorities("read") // we can add "ROLE_ADMIN" to this and keep both
+                .build();
+
+        var u2 = User.withUsername("john")
+                .password(passwordEncoder().encode("12345"))
+                .authorities("write")
+                .build();
+
+        var u3 = User.withUsername("jake")
+                .password(passwordEncoder().encode("12345"))
+                .roles("MANAGER") // equivalent with an authority named ROLE_MANAGER
+                .build();
+
 
         uds.createUser(u1);
+        uds.createUser(u2);
+        uds.createUser(u3);
 
         return uds;
     }
